eeeae5455d21ed070ae58738a6873c6d64069035
Onur Çağan <onurcagann@gmail.com>
2023-07-20 15:58:22 +0300

    adding in some parallax effects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,30 @@
+'use client'
+
 import ParallaxHome from '@/components/ParallaxHome'
+import { motion, useScroll, useSpring } from 'framer-motion'
 
 export default function Home() {
+  const { scrollYProgress } = useScroll()
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  })
   return (
     <div className='flex flex-col w-[100vw] h-[100vh]'>
       <header className='absolute top-0 flex flex-row justify-center w-full gap-2 mx-auto'>
-        <button>placeHolder</button>
-        <button>placeHolder</button>
-        <button>placeHolder</button>
-        <button>placeHolder</button>
+        <button className='w-32 p-2 m-2 bg-purple-600 rounded-md'>Home</button>
+        <button className='w-32 p-2 m-2 bg-purple-600 rounded-md'>About</button>
+        <button className='w-32 p-2 m-2 bg-purple-600 rounded-md'>Projects</button>
+        <button className='w-32 p-2 m-2 bg-purple-600 rounded-md'>Contact</button>
       </header>
-      <main className='flex items-center justify-center w-full my-auto text-center'>
-        <ParallaxHome />
+      <main className='flex flex-col items-center justify-center w-full my-auto mt-4 text-center -z-10'>
+        <ParallaxHome type={'car'} />
+        <ParallaxHome type={'book'} />
+        <ParallaxHome type={'hospital'} />
+        <ParallaxHome type={'site'} />
+        <ParallaxHome type={'farm'} />
+        <motion.div className='fixed left-0 right-0 h-[5px] bottom-0 bg-white' style={{ scaleX }} />
       </main>
     </div>
   )
--- src/components/ParallaxHome.tsx
+++ src/components/ParallaxHome.tsx
@@ -1,11 +1,24 @@
 'use client'
-import { motion } from 'framer-motion'
-import React from 'react'
 
-const ParallaxHome = () => {
+import { MotionValue, motion, useScroll, useTransform } from 'framer-motion'
+import Image from 'next/image'
+import React, { useRef } from 'react'
+
+interface IParallaxHome {
+  type: string
+}
+
+const ParallaxHome = ({ type }: IParallaxHome) => {
+  function useParallax(value: MotionValue<number>, distance: number) {
+    return useTransform(value, [0, 1], [-distance, distance])
+  }
+
+  const ref = useRef(null)
+  const { scrollYProgress } = useScroll({ target: ref })
+  const y = useParallax(scrollYProgress, 300)
   return (
     <>
-      <motion.div
+      {/* <motion.div
         animate={{
           scale: [1, 2, 2, 1, 1],
           rotate: [0, 0, 270, 270, 0],
@@ -13,7 +26,23 @@ const ParallaxHome = () => {
         }}
         transition={{ repeat: 3, delay: 2, duration: 1.5 }}
         className='rounded-full w-96 h-96 bg-gradient-to-r from-green-400 to-blue-500'
-      />
+      /> */}
+
+      <section className='relative w-[100vw] h-[100vh] flex justify-center items-center'>
+        <div ref={ref}>
+          <Image
+            alt={`Background img of ${type} type`}
+            src={`https://source.unsplash.com/random/1920x1080/?${type}`}
+            width={1920}
+            height={1080}
+            className='w-[90%] h-[90%]'
+          />
+        </div>
+        <motion.h2 style={{ y }} className='text-red-500 text-7xl'>
+          {`#${type.charAt(0).toUpperCase() + type.slice(1)}`}
+        </motion.h2>
+        <div className='absolute inset-0 bg-black opacity-40' />
+      </section>
     </>
   )
 }
