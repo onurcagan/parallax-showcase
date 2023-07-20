591495de5cf2271da92c3444f05c1211e2d5c90d
Onur Çağan <onurcagann@gmail.com>
2023-07-20 14:46:28 +0300

    set up homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@ import ParallaxHome from '@/components/ParallaxHome'
 export default function Home() {
   return (
     <div className='flex flex-col w-[100vw] h-[100vh]'>
-      <header className='top-0 flex flex-row justify-center gap-2'>
-        <div>button</div>
-        <div>button</div>
-        <div>button</div>
-        <div>button</div>
+      <header className='absolute top-0 flex flex-row justify-center w-full gap-2 mx-auto'>
+        <button>placeHolder</button>
+        <button>placeHolder</button>
+        <button>placeHolder</button>
+        <button>placeHolder</button>
       </header>
       <main className='flex items-center justify-center w-full my-auto text-center'>
         <ParallaxHome />
--- src/components/ParallaxHome.tsx
+++ src/components/ParallaxHome.tsx
@@ -1,8 +1,21 @@
 'use client'
+import { motion } from 'framer-motion'
 import React from 'react'
 
 const ParallaxHome = () => {
-  return <>Testing</>
+  return (
+    <>
+      <motion.div
+        animate={{
+          scale: [1, 2, 2, 1, 1],
+          rotate: [0, 0, 270, 270, 0],
+          borderRadius: ['20%', '20%', '50%', '50%', '20%'],
+        }}
+        transition={{ repeat: 3, delay: 2, duration: 1.5 }}
+        className='rounded-full w-96 h-96 bg-gradient-to-r from-green-400 to-blue-500'
+      />
+    </>
+  )
 }
 
 export default ParallaxHome
